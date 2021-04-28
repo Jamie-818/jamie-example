9f277514da9c3ccc194987dfe6c660f9e355fa36
禤伟耀 <xuanweiyao@syni.com>
2021-04-28 11:11:59 +0800

    feature: 创建IO模块

---
+++ IO/src/main/java/jamie/example/io/nio/CreateBufferDemo.java
@@ -0,0 +1,28 @@
+package jamie.example.io.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 创建 Buffer demo
+ * @author jamie
+ */
+public class CreateBufferDemo {
+
+    public static void main(String[] args) {
+        //1.创建一个指定长度的缓冲区, 以ByteBuffer为例
+        ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+        for(int i = 0; i < 5; i++){
+            System.out.println(byteBuffer.get());
+        }
+        //在此调用会报错
+        // System.out.println(byteBuffer.get());
+        System.out.println("-------------");
+        //2.创建一个有内容的缓冲区
+        ByteBuffer wrap = ByteBuffer.wrap("jamie".getBytes());
+        for(int i = 0; i < 5; i++){
+            System.out.println(wrap.get());
+        }
+        // System.out.println(wrap.get());
+    }
+
+}
